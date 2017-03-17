ccd9e27ed392b4173d7379608047d328df5868d3
reno- <contact@rubiano.fr>
2017-03-17 17:35:06 +0100

    come on

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,13 +6,15 @@ case "$TRAVIS_OS_NAME" in
   linux)
     sudo apt-get -y install python3 python3-setuptools libpython3.6-dev 
     sudo easy_install3 pip
+	pip install pyqt5
+	pip install -v git+https://github.com/PixelStereo/pyossia.git@master
+	sudo pip install pyinstaller pyqt5 zeroconf
   ;;
   osx)
     brew install python3
     brew link --overwrite python3
+    pip3 install pyqt5
+	pip3 install -v git+https://github.com/PixelStereo/pyossia.git@master
+	sudo pip3 install pyinstaller pyqt5 zeroconf
   ;;
 esac
-
-pip3 install pyqt5
-pip3 install -v git+https://github.com/PixelStereo/pyossia.git@master
-sudo pip3 install pyinstaller pyqt5 zeroconf
