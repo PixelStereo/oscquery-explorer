5587b46775053ec9809064b1b80179bb68b7ec1b
reno- <contact@rubiano.fr>
2017-08-31 17:23:37 +0200

    simplification

--- src/main.py
+++ src/main.py
@@ -11,28 +11,16 @@ from PyQt5.QtCore import QFileInfo
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication
 
-try:
-    # stylesheet
-    import qdarkstyle
-except Exception as error:
-    print('failed ' + str(error))
-
 
 if __name__ == "__main__":
-    # this is for python2 only
+    # python2 only
     try:
         reload(sys)
         sys.setdefaultencoding('utf8')
     except NameError:
         pass
+    # end python 2 only
     app = QApplication(sys.argv)
-    try:
-        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    except Exception as error:
-        print('failed ' + str(error))
-    root = QFileInfo(__file__).absolutePath()
-    path = root+'/icon/icon.png'
-    app.setWindowIcon(QIcon(path))
     mainWin = MainWindow()
     mainWin.show()
     sys.exit(app.exec_())
